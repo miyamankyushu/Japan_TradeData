24647e1816b143b50cf11abf1fcef81dbdbcaee0
miyamankyushu <watari.hiroki.0499@gmail.com>
2025-04-22 01:28:38 +0900

    s

--- library/HScode_scrape.py
+++ library/HScode_scrape.py
@@ -8,11 +8,10 @@ from io import StringIO
 
 
 
-def generate_customs_urls(year, month, No_range):
+def generate_customs_urls(year, month, nu_range):
     urls = []
     print(1)
-    for No in No_range:
-        print(No)
+    for No in nu_range:
         # 77類は存在しないのでスキップ
         if No == 77:
             continue
